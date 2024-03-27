d3908f2e5a1b5aa34c29456bd4d5cc3aeab4b1ff
Shpunka <mifaizullin@edu.hse.ru>
2024-03-27 15:18:46 +0300

    da

---
+++ cmake-build-debug/CMakeFiles/aip_project_2_buhs.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aip_project_2_buhs.dir/main.cpp.obj"
+  "CMakeFiles/aip_project_2_buhs.dir/main.cpp.obj.d"
+  "CMakeFiles/aip_project_2_buhs.dir/neural_net.cpp.obj"
+  "CMakeFiles/aip_project_2_buhs.dir/neural_net.cpp.obj.d"
+  "CMakeFiles/aip_project_2_buhs.dir/program.cpp.obj"
+  "CMakeFiles/aip_project_2_buhs.dir/program.cpp.obj.d"
+  "aip_project_2_buhs.exe"
+  "aip_project_2_buhs.exe.manifest"
+  "aip_project_2_buhs.pdb"
+  "libaip_project_2_buhs.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aip_project_2_buhs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/aip_project_2_buhs.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for aip_project_2_buhs.
