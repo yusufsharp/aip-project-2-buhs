d26a8f116381f277f4efd2790bf054cf322e5fc0
yusufsharp <zxces@vk.com>
2024-05-26 23:34:38 +0300

    Что-то добавлено, но есть проблемы

--- TensorsNet.h
+++ TensorsNet.h
@@ -2,12 +2,14 @@
 // Created by zxces on 26.03.2024.
 //
 #pragma once
+
 #include "matrix.h"
+
 #ifndef AIP_PROJECT_2_BUHS_TENSORSNET_H
 #define AIP_PROJECT_2_BUHS_TENSORSNET_H
 
 
-struct Layer{
+struct Layer {
     Matrix weights;
     Matrix d_weights;
     std::vector<double> biases;
@@ -21,17 +23,31 @@ struct Layer{
 
 class TensorsNet {
 public:
-    explicit TensorsNet(const std::vector<size_t> & all_layers);
-    void forwardPass(const std::vector<double> & inputs);
-    void backwardPass(const std::vector<double> & outputs,  double  lr, double moment);
+    TensorsNet();
+
+    explicit TensorsNet(const std::vector<size_t> &all_layers);
+
+    void forwardPass(const std::vector<double> &inputs);
+
+    void backwardPass(const std::vector<double> &outputs, double lr, double moment);
+
     void addConvLayer(size_t num_filter, size_t filters_size);
+
     void addLayer(size_t num_input, size_t num_output, size_t layer_type);
+
     void convForward(const std::vector<double> &inputs, size_t conv_index);
+
+    void convBackward(size_t conv_index, std::vector<double> before_grads, bool last, double lr, double moment);
+
+    std::vector<std::vector<Layer>> graph;
+
 private:
     //с учетом фильтра
-    std::vector<std::vector<Layer>> graph;
-    static void relu_function(std::vector<double> & nonactive);
+
+    static void relu_function(std::vector<double> &nonactive);
+
     static std::vector<double> relu_function_derived(const std::vector<double> &nonderived);
+
 };
 
 
